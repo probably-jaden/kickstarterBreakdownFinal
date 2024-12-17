94dc03661fa88f0dccf71181084897b1ce5d0c8d
Jaden Earl <jadendwightearl@gmail.com>
2024-12-17 13:39:45 -0700

    updated from pull

--- final/preprocessing.py
+++ final/preprocessing.py
@@ -18,8 +18,6 @@ def load_data():
         - 'usd_exchange_rate' : The exchange rate to USD.
         - 'usd_pledged' : The amount pledged in USD.
         - 'category' : The project category.
-        - 'urls' : URL information about the project.
-        - 'creator' : Information about the project's creator.
         - 'goal' : The funding goal for the project.
         - 'deadline' : The project deadline.
         - 'created_at' : The project creation date.
@@ -31,16 +29,16 @@ def load_data():
     --------
     >>> df = load_data()
     >>> df.head()
-       usd_exchange_rate  usd_pledged     category                    urls  ...  name  backers_count
-    0               1.0         500.0  technology   {'web': 'example.com'}  ...   App           10
-    1               1.2         300.0       music  {'web': 'example.org'}   ...  Song            5
+       usd_exchange_rate  usd_pledged     category   ...  name  backers_count
+    0               1.0         500.0  technology    ...   App           10
+    1               1.2         300.0       music    ...  Song            5
     """
     df_combined = pd.read_csv("kickstarterData.csv")
     df_combined['usd_exchange_rate'] = pd.to_numeric(df_combined['usd_exchange_rate'], errors='coerce')
     df_combined['usd_pledged'] = pd.to_numeric(df_combined['usd_pledged'], errors='coerce')
     df_combined['goal'] = pd.to_numeric(df_combined['goal'], errors='coerce')
 
-    df_combined = df_combined[['usd_exchange_rate', 'usd_pledged', 'category', 'urls', 'creator', 'goal', 
+    df_combined = df_combined[['usd_exchange_rate', 'usd_pledged', 'category', 'goal', 
                                'deadline', 'created_at', 'slug', 'name', 'backers_count']].drop_duplicates(subset=['slug'])
     return df_combined
 
